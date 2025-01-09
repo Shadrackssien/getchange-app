cacbb97d5df6cff20b4a653dc6e935fc7cd9f8a8
Shadrack Essien <shadrack6960@gmail.com>
2025-01-09 17:46:37 +0000

    created the profile dropdown list

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,25 @@
 <script>
 import getchange from "../assets/icons/getchange.png";
 import arrowdown from "../assets/icons/arrow-down.png";
+import logoutIcon from "../assets/icons/log-out.png";
+import settings from "../assets/icons/settings.png";
+import timeIcon from "../assets/icons/time.png";
 export default {
   data() {
     return {
       getchange,
       arrowdown,
       user: null,
+      isDropdownVisible: false,
+      dropdowns: [
+        {
+          src: timeIcon,
+          label: "Wallet History",
+          route: "/dashboard",
+        },
+        { src: settings, label: "Settings", route: "/dashboard" },
+        { src: logoutIcon, label: "Logout", route: "/dashboard" },
+      ],
     };
   },
   created() {
@@ -15,21 +28,52 @@ export default {
       this.user = JSON.parse(userData);
     }
   },
+  methods: {
+    toggleDropdown() {
+      this.isDropdownVisible = !this.isDropdownVisible;
+    },
+  },
 };
 </script>
 <template>
   <div
-    class="flex justify-between w-full h-max bg-white border-b-2 border-b-[#6A7E8A1A] p-2"
+    class="flex justify-between w-full text-[#013C61] h-max bg-white border-b-2 border-b-[#6A7E8A1A] p-2"
   >
-    <div>
+    <div
+      class="hover:scale-110 transition-all duration-300 ease-in-out cursor-pointer"
+    >
       <img :src="getchange" alt="logo" />
     </div>
-    <div class="flex items-center space-x-2 px-6">
+    <div
+      @click="toggleDropdown"
+      class="cursor-pointer flex items-center space-x-2 px-10"
+    >
       <div class="size-7 bg-[#6A7E8A] rounded-full"></div>
       <div class="flex items-center space-x-2">
         <p>Hi, {{ user.username }}</p>
         <img :src="arrowdown" alt="arrow icon" />
       </div>
+    </div>
+
+    <!-- Dropdown menu -->
+    <div
+      v-if="isDropdownVisible"
+      class="absolute bg-white shadow-lg w-44 right-6 top-[60px] py-6 border border-gray-200"
+    >
+      <ul
+        v-for="dropdown in dropdowns"
+        :key="dropdown.label"
+        class="space-y-2.5"
+      >
+        <router-link :to="dropdown.route">
+          <li
+            class="flex items-center gap-2 py-2 px-4 hover:bg-gray-100 cursor-pointer"
+          >
+            <img :src="dropdown.src" alt="time icon" />
+            <p>{{ dropdown.label }}</p>
+          </li>
+        </router-link>
+      </ul>
     </div>
   </div>
 </template>
